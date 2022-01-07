ecbe197cf81b745d7c79a46586f5efd7a1dd0358
sshong11 <sshong2016@gmail.com>
2022-01-07 18:59:30 -0500

    added unique usernames

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
             token = encode_token({user_id: @user.id})
             render json: {user: @user, token: token}
         else
-            render json: {error: "Invalid username or password"}
+            render json: {error: "Invalid username or password or Username already taken"}
         end
     end
 
